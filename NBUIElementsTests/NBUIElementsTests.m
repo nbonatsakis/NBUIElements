e85f65c9b4aa1605c1d688b985def71c0e9a57f6
Nick Bonatsakis <nbonatsakis@gmail.com>
2011-09-21 11:48:04 -0400

    Initial Commit

---
+++ NBUIElementsTests/NBUIElementsTests.m
@@ -0,0 +1,32 @@
+//
+//  NBUIElementsTests.m
+//  NBUIElementsTests
+//
+//  Created by nbonatsakis on 9/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NBUIElementsTests.h"
+
+@implementation NBUIElementsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NBUIElementsTests");
+}
+
+@end
